17a9652fa6f80d0852e56fa7f2aaaa64ff5337dd
qrowned <admin@qrowned.dev>
2022-02-01 23:38:18 +0100

    Add async proxy check on LoginEvent

--- xvpnchecker-velocity/src/main/java/dev/qrowned/vpnchecker/velocity/listener/LoginListener.java
+++ xvpnchecker-velocity/src/main/java/dev/qrowned/vpnchecker/velocity/listener/LoginListener.java
@@ -1,19 +1,19 @@
 package dev.qrowned.vpnchecker.velocity.listener;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.velocitypowered.api.event.EventTask;
 import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.ResultedEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.LoginEvent;
 import com.velocitypowered.api.proxy.Player;
 import dev.qrowned.vpnchecker.common.VPNCheckerPlugin;
-import dev.qrowned.vpnchecker.common.handler.ProxyCheckHandler;
-import dev.qrowned.vpnchecker.common.utils.VPNCheckerMessages;
 import dev.qrowned.vpnchecker.common.config.JsonConfig;
 import dev.qrowned.vpnchecker.common.config.impl.PluginConfig;
+import dev.qrowned.vpnchecker.common.handler.ProxyCheckHandler;
 import dev.qrowned.vpnchecker.common.ip.IPCheck;
 import dev.qrowned.vpnchecker.common.ip.Operator;
 import dev.qrowned.vpnchecker.common.result.ProxyCheckResult;
+import dev.qrowned.vpnchecker.common.utils.VPNCheckerMessages;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.jetbrains.annotations.NotNull;
@@ -29,23 +29,25 @@ public final class LoginListener {
     }
 
     @Subscribe(order = PostOrder.FIRST)
-    public void handle(@NotNull LoginEvent event) {
+    public EventTask handle(@NotNull LoginEvent event) {
         final Player player = event.getPlayer();
         if (this.pluginConfig.getConfig()
-                .getWhitelistedIps().contains(player.getUniqueId())) return;
+                .getWhitelistedIps().contains(player.getUniqueId())) return null;
 
         final String hostAddress = player.getRemoteAddress().getAddress().getHostAddress();
 
-        final ProxyCheckResult proxyCheckResult = this.proxyCheckHandler.getProxyCheckResult(hostAddress);
-        final IPCheck ipCheck = proxyCheckResult.getIpCheck();
-        if (ipCheck == null || !ipCheck.isProxy()) return;
+        return EventTask.async(() -> {
+            final ProxyCheckResult proxyCheckResult = this.proxyCheckHandler.getProxyCheckResult(hostAddress);
+            final IPCheck ipCheck = proxyCheckResult.getIpCheck();
+            if (ipCheck == null || !ipCheck.isProxy()) return;
 
-        final Operator operator = ipCheck.getOperator();
-        final TextComponent component = LegacyComponentSerializer.legacySection().deserialize(operator == null
-                ? VPNCheckerMessages.VPN_UNKNOWN_OPERATOR
-                : String.format(VPNCheckerMessages.VPN_OPERATOR, operator.getName()));
+            final Operator operator = ipCheck.getOperator();
+            final TextComponent component = LegacyComponentSerializer.legacySection().deserialize(operator == null
+                    ? VPNCheckerMessages.VPN_UNKNOWN_OPERATOR
+                    : String.format(VPNCheckerMessages.VPN_OPERATOR, operator.getName()));
 
-        event.setResult(ResultedEvent.ComponentResult.denied(component));
+            event.setResult(ResultedEvent.ComponentResult.denied(component));
+        });
     }
 
 }
